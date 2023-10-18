3945645c6e7c713d8fc4f2035efa702126f23e85
Nikhitha-kanne <nikhitha.kanne@springml.com>
2023-10-18 15:00:15 +0530

    updated dataflow terraform script

--- terraform/dataflow/variables.tf
+++ terraform/dataflow/variables.tf
@@ -9,43 +9,43 @@ variable "dataflow_region" {
 }
 
 variable "template_gcs_path" {
-    description = "GCS path of Dataflow Flex template image"
-    type=string
+  description = "GCS path of Dataflow Flex template image"
+  type        = string
 }
 
 variable "temp_gcs_location" {
-    description = "Dataflow temporary location"
-    type=string 
+  description = "Dataflow temporary location"
+  type        = string
 }
 
 variable "dataflow_staging_location" {
   description = "Dataflow staging location"
-  type=string
+  type        = string
 }
 
 variable "dataflow_job_parameters" {
-    description = "Dataflow Flex template parameters"
-    type = list(object({
-        input_topic:string,
-        input_subscription:string,
-        output_path:string
-    }))
+  description = "Dataflow Flex template parameters"
+  type = list(object({
+    input_topic : string,
+    input_subscription : string,
+    output_path : string
+  }))
 }
 
-variable "dataflow_vpc_network"{
+variable "dataflow_vpc_network" {
   description = "Network to be used by dataflow workers"
-  type=string
+  type        = string
   # default = "default"
 
 }
 
 variable "dataflow_subnetwork_selflink" {
-  description="Subnetwork self link to be used by dataflow wroker VMs"
-  type=string
-  
+  description = "Subnetwork self link to be used by dataflow wroker VMs"
+  type        = string
+
 }
 
 variable "dataflow_serviceaccount" {
   description = "Dataflow service account"
-  type = string
+  type        = string
 }
