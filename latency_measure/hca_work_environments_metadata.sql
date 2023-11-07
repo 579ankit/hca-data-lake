ebe8b00fd59d391a0322b480c20f4b2ec4046704
Anchit <anchit.mallick@springml.com>
2023-11-07 12:14:22 +0530

    Created the ddl for all the batch latency measure

---
+++ latency_measure/hca_work_environments_metadata.sql
@@ -0,0 +1,20 @@
+CREATE EXTERNAL TABLE `hca-usr-hin-landing-datalake.hca_employee_data_landing.hca_work_environments_metadata`
+WITH CONNECTION `hca-usr-hin-landing-datalake.us-central1.employee_data_biglake_connection`
+OPTIONS(
+  object_metadata = 'SIMPLE',
+  uris=['gs://hca_employee-data_landing_20231005/work_environments/*.csv']
+);
+
+
+WITH times AS (
+    SELECT 
+        CAST(DATETIME(
+          CONCAT(
+                REGEXP_EXTRACT(uri, r'date=(\d{4}-\d{2}-\d{2})'), ' ',
+                REGEXP_EXTRACT(uri, r'time=(\d{2}:\d{2})'),  ':00'
+         ) ) AS DATETIME
+        ) AS st,
+        CAST(updated AS DATETIME) as lt
+    FROM hca-usr-hin-landing-datalake.hca_employee_data_landing.hca_work_environments_metadata
+)
+SELECT AVG(TIMESTAMP_DIFF(lt, st, SECOND)) FROM times;
